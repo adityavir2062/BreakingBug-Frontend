628efaf622353dd13869b817beed48a0d488e0d2
adityavir2062 <121630514+adityavir2062@users.noreply.github.com>
2024-08-03 12:44:09 +0530

    Update Slide.jsx
    Made changes into the code !

--- src/pages/Slide.jsx
+++ src/pages/Slide.jsx
@@ -1,6 +1,6 @@
 import { Divider, Box, Typography, Button, styled, Container } from '@mui/material';
-
-import { Link, useNavigate } from 'react-router';
+import {Styled} from '@mui/material' ;
+import { Link, useNavigate } from 'react-router-dom';
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
 
@@ -121,7 +121,7 @@ const Text = styled(Typography)`
 const TextContainer = styled(Container)`
     display: flex;
     gap: 8px;
-    justify-content: center;
+    justifyContent: center;
     align-items: center;
     margin: 8px;
 `;
